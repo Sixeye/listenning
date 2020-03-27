42abc4e3015814243e6d27e6382cd494f4b0aece
Sixeye <srinath@srinath.fr>
2020-03-27 07:07:45 +0100

    server

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+/*
+*Primary files for the API
+*
+*/
+
+//Dependencies
+const http = require('http');
+const url  = require('url');
+
+// The server should respond to all requests with a string
+let server = http.createServer(function (req, res) {
+    // Get the url and parse it
+    let parsedUrl = url.parse(req.url,true);
+
+    // Get the path
+    let path = parsedUrl.pathname;
+    let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+    // Send the response
+    res.end('Hallo friends from  : '+trimmedPath);
+
+    // Log the request path
+    console.log('Request received on path: '+trimmedPath);
+});
+
+// Start the server and have it listenning on port 3000
+
+server.listen(3000, function () {
+    console.log("The server is listenning on port 3000")
+})
